ca346c2120e938858f50846efc34a7a143c3b2e4
evalor <mipone@foxmail.com>
2017-12-08 00:05:11 +0800

    2.0 Refactoring

---
+++ src/MIME.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace easySwoole\VerifyCode;
+
+/**
+ * 验证码MIME类型
+ * Class MIME
+ * @author : evalor <master@evalor.cn>
+ * @package easySwoole\VerifyCode
+ */
+class MIME
+{
+    const JPG = 'image/jpeg';
+    const PNG = 'image/png';
+    const BMP = 'image/bmp';
+    const GIF = 'image/gif';
+}
