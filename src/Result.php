6766ba935fe39d7bbb6344f5123d36abd9c914bd
evalor <mipone@foxmail.com>
2017-12-08 00:21:09 +0800

    update

--- src/Result.php
+++ src/Result.php
@@ -9,35 +9,63 @@
 
 namespace easySwoole\VerifyCode;
 
+/**
+ * 验证码结果类
+ * Class Result
+ * @author : evalor <master@evalor.cn>
+ * @package easySwoole\VerifyCode
+ */
 class Result
 {
-    private $imageBody;
-    private $imageStr;
+    private $CaptchaByte;  // 验证码图片
+    private $CaptchaMime;  // 验证码类型
+    private $CaptchaCode;  // 验证码内容
 
-    function __construct($image, $str)
+    function __construct($Byte, $Code, $Mime)
     {
-        $this->imageBody = $image;
-        $this->imageStr = $str;
+        $this->CaptchaByte = $Byte;
+        $this->CaptchaMime = $Mime;
+        $this->CaptchaCode = $Code;
     }
 
     /**
+     * 获取验证码图片
+     * @author : evalor <master@evalor.cn>
      * @return mixed
      */
-    public function getImageBody()
+    function getImageByte()
     {
-        return $this->imageBody;
+        return $this->CaptchaByte;
     }
 
     /**
-     * @return mixed
+     * 返回图片Base64字符串
+     * @author : evalor <master@evalor.cn>
+     * @return string
      */
-    public function getImageStr()
+    function getImageBase64()
     {
-        return $this->imageStr;
+        $base64Data = base64_encode($this->CaptchaByte);
+        $Mime = $this->CaptchaMime;
+        return "data:{$Mime};base64,{$base64Data}";
     }
 
-    function getMineType()
+    /**
+     * 获取验证码内容
+     * @author : evalor <master@evalor.cn>
+     * @return mixed
+     */
+    function getImageCode()
     {
-        return 'image/png';
+        return $this->CaptchaCode;
+    }
+
+    /**
+     * 获取Mime信息
+     * @author : evalor <master@evalor.cn>
+     */
+    function getImageMime()
+    {
+        return $this->CaptchaMime;
     }
 }
